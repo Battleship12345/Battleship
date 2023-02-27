4f7c844801e15244b6d483c2b2a89b397235202d
HeiHalloHade <126481599+HeiHalloHade@users.noreply.github.com>
2023-02-27 14:47:45 +0100

    Update battleship.py

--- Battleship/battleship.py
+++ Battleship/battleship.py
@@ -2,7 +2,7 @@ import pygame
 from sys import exit
 from random import randint, choice
 
-size = width, height = (800, 400)
+size = width, height = (600, 600)
 fps = 30
  
 pygame.init()
@@ -11,5 +11,22 @@ pygame.display.set_caption("Battleship")
 clock = pygame.time.Clock()
 
 
-pygame.display .update()
-clock.tick(fps)
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y, x2):
+        super().__init__()
+        self.y = y
+        self.x = x
+        self.width = x2
+        self.image = pygame.Surface((self.width, 5))
+        self.image.fill((255,255,255))
+        self.rect = self.image.get_rect(topleft = (x,y))
+
+
+
+
+while True:
+    screen.fill("blue")
+    
+    pygame.display .update()
+    clock.tick(fps)
