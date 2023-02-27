90100173761517874918ddb150b62d6232556afd
HeiHalloHade <126481599+HeiHalloHade@users.noreply.github.com>
2023-02-27 14:33:09 +0100

    Update battleship.py

--- Battleship/battleship.py
+++ Battleship/battleship.py
@@ -1,3 +1,15 @@
 import pygame
 from sys import exit
 from random import randint, choice
+
+size = width, height = (800, 400)
+fps = 30
+ 
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Battleship")
+clock = pygame.time.Clock()
+
+
+pygame.display .update()
+clock.tick(fps)
